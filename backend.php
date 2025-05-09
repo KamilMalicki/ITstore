7cd44a5a219ff6a4eeff547ee6cc3a08c27af562
KamilMalicki <146217192+KamilMalicki@users.noreply.github.com>
2025-05-09 10:42:46 +0200

    dodanie backend

---
+++ backend.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: application/json');
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (file_put_contents('products.json', json_encode($data['products'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE))) {
+    echo json_encode(['success' => true]);
+} else {
+    http_response_code(500);
+    echo json_encode(['error' => 'Błąd zapisu']);
+}
+?>
